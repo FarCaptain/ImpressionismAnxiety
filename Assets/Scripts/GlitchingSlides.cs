19e135da78be1b79b91786b210048a85a0a4ebf6
Sphinx <quyuan2008@163.com>
2021-11-05 01:30:51 -0600

    add glitching slide + space to test stop it + double decrease of sanity

--- Assets/Scripts/GlitchingSlides.cs
+++ Assets/Scripts/GlitchingSlides.cs
@@ -14,18 +14,19 @@ public class GlitchingSlides : MonoBehaviour
     private Renderer slideRenderer;
     private int randomTextureIndex;
     private int goodPerioud;
+    private float timeCounter = 0.0f;
 
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         slideRenderer = slide.GetComponent<Renderer>();
-        goodPerioud = Random.Range(30, 200);
+        goodPerioud = Random.Range(100, 300);
 
         // gameObject.GetComponent<>
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         // randomTextureIndex = Random.Range(0, textures.Length);
         // slideRenderer.material.mainTexture = textures[randomTextureIndex];
@@ -38,6 +39,24 @@ public class GlitchingSlides : MonoBehaviour
             randomTextureIndex = Random.Range(0, textures.Length);
             slideRenderer.material.mainTexture = textures[randomTextureIndex];
             slideRenderer.material.SetTexture ("_EmissionMap", textures[randomTextureIndex]);
+
+            // if not resolved, extra decrease
+            timeCounter += Time.deltaTime;
+            if (timeCounter >= 1.0f)
+            {
+                BreathGameplayController.score -=2;
+                timeCounter = 0.0f;
+            }
+            
+
+
+            // Place holder
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                goodPerioud = Random.Range(200, 1000);
+                slideRenderer.material.mainTexture = textures[0];
+                slideRenderer.material.SetTexture ("_EmissionMap", textures[0]);
+            }
         }
     }
 }
